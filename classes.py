ba2117f9213022cd32a473120e5da5929184d9e4
Neiso <djulian.ouatla@outlook.com>
2020-03-21 22:32:14 +0100

    Add the camera class

---
+++ classes.py
@@ -0,0 +1,37 @@
+from global_variables import *
+import math
+import pygame
+from pygame.locals import *
+
+class Camera():
+	def __init__(self, init_pos=(0, 0, 0), rot=(0,0)):
+		self.x = init_pos[0]
+		self.y = init_pos[1]
+		self.z = init_pos[2]
+		self.rot_xy = rot[0]
+		self.rot_xz = rot[1]
+	
+	def rotate_xy(self, dt, x, y):
+		x2 = x * math.cos(dt * self.rot_xy) - y * math.sin(dt * self.rot_xy)
+		y2 = x * math.sin(dt * self.rot_xy) + y * math.cos(dt * self.rot_xy)
+		return (x2, y2)
+
+	def rotate_xz(self, dt, x, z):
+		x2 = x * math.cos(dt * self.rot_xz) - z * math.sin(dt * self.rot_xz)
+		z2 = x * math.sin(dt * self.rot_xz) + z * math.cos(dt * self.rot_xz)
+		return (x2, z2)
+
+	def update(self, dt, key):
+		if key[pygame.K_d]: 	self.x += dt
+		if key[pygame.K_q]:		self.x -= dt
+		if key[pygame.K_z]:		self.y -= dt
+		if key[pygame.K_s]:		self.y += dt
+		if key[pygame.K_a]:		self.z += dt
+		if key[pygame.K_e]:		self.z -= dt
+
+		if key[pygame.K_o]:		self.rot_xy += 1
+		if key[pygame.K_p]:		self.rot_xy -= 1
+
+		if key[pygame.K_l]:		self.rot_xz += 1
+		if key[pygame.K_m]:		self.rot_xz -= 1
+
