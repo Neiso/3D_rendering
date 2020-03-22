9d3452e99e1b6835c38642622dbe2c04cd5963e1
Neiso <djulian.ouatla@outlook.com>
2020-03-22 17:15:12 +0100

    Adding pyramide and pyramide above a cube

--- main.py
+++ main.py
@@ -1,21 +1,34 @@
 import pygame
 from classes import Camera
+from function import *
 from pygame.locals import *
 from global_variables import *
 
+"	INIT THE PYGAME MODULE	"
+
+pygame.init()
+
+"	INIT THE WINDOW, FULL SCREEN AND PREPARE CLOCK FOR 60 FPS	"
+
 screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 pygame.display.set_caption("My first program !")
+pygame.display.toggle_fullscreen()
 clock = pygame.time.Clock()
 
-camera = Camera((0, 0, 5))
+"	INIT THE CAMERA (USER's VIEUW) AND SET THE ROTATION		"
+
+camera = Camera((0, 0, 5), (0, 0))
 rad = 0
+
+"	MAIN ENGINE		"
 
 while (ON):
 	dt = clock.tick(60)/1000
 	screen.fill(BLACK)
 
 	key = pygame.key.get_pressed()
-	camera.update(dt, key)
+	camera.update(0.01, key)
+	# camera.rot_yz += 1
 
 	for event in pygame.event.get():
 		if event.type == pygame.QUIT:
@@ -23,22 +36,9 @@ while (ON):
 		elif key[pygame.K_ESCAPE]:
 			ON = False
 
-	for edge in edges2:
-		points = []
-		for x, y, z in [verts2[edge[0]], verts2[edge[1]]]:
-			x, y = camera.rotate_xy(0.02, x, y)
-			x, z = camera.rotate_xz(0.02, x, z)
-			x += camera.x
-			y += camera.y
-			z += camera.z
-			
-			if z == 0:
-				z = 1
-			f = 600 / z
-			x, y = int(f*x), int(f*y)
-			points += [(center_x + x, center_y + y)]
-		pygame.draw.line(screen, WHITE, points[0], points[1], 3)
+	draw_shape(cube_edges, cube_verts, camera, screen)
 
+	screen.blit(update_fps(clock), (10,0))
 	pygame.display.flip()
 	rad += 0.0002
 	
